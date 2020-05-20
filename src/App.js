ce9015f80db53d01839b975fd08daa5726f37af8
Gulzada Bolotova <64300098+GulzadaBolotova@users.noreply.github.com>
2020-05-20 20:54:51 +0600

    Add toolbar components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./App.css";
+import Logo from "./components/Logo/Logo";
+import Nav from "./components/Nav/Nav";
+import CartLink from "./components/CartLink/CartLink";
 
 function App() {
-  return <div className="App">Book Shop by Gulzada Bolotova</div>;
+  return (
+    <div className="App">
+      <nav>
+        <Logo />
+        <Nav />
+        <CartLink />
+      </nav>
+      <main>Book Shop by Bolotova Gulzada</main>
+    </div>
+  );
 }
 export default App;
