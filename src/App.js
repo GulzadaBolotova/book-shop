e2ff6b69b7a15e7053513af69dbc309cb3a4ea63
Gulzada Bolotova <64300098+GulzadaBolotova@users.noreply.github.com>
2020-05-20 22:38:54 +0600

    Stylize toobar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,56 @@ import Layout from "./containers/Layout/Layout";
 export default () => {
   return (
     <div className="App">
-      <Layout>Book Shop by Bolotova Gulzada</Layout>
+      <Layout>
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+        Book Shop by Bolotova Gulzada
+        <br />
+      </Layout>
     </div>
   );
 };
